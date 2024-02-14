9eb41abedeee1a5cc28512cba5acff9c0d4625bd
SathishMani219 <sathish.kumarmani@education.gov.uk>
2024-02-14 21:00:09 +0000

    push actions

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,6 +2,7 @@ locals {
   ###########
   # tfstate storage container #
   ###########
-tfstate_container_name = var.tfstate_containername
-azurerm_terraform_storage_account = var.azureterraformstorageaccount
+tfstate_container_name = var.tfstate_storage_container_name
+azurerm_terraform_storage_account = var.tf_state_storage_account
 }
+#
