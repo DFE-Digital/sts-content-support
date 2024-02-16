512553d7dde3941f985dcc8a1768e73952477bd1
SathishMani219 <sathish.kumarmani@education.gov.uk>
2024-02-16 14:30:55 +0000

    update Terraform for keyvault

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,6 +14,37 @@ variable "tf_state_storage_account"{
   description = "Terraform TFstate storage account name"
 }
 
+variable "resource_group_name"{
+  type = string
+  description = "Resource Group name"
+}
+
+variable "project_name"{
+  type = string
+  description = "Project name"
+}
+
+variable "environment" {
+  type = string
+  description  = "Environment name "
+}
+
+variable "azure_location" {
+  description = "Recourse location"
+  type        = string
+}
+
+variable "az_tag_environment" {
+  description = "Environment tag to be applied to all resources"
+  type        = string
+}
+
+variable "az_tag_product" {
+  description = "Product tag to be applied to all resources"
+  type        = string
+}
+
+
 variable "client_id" {
   type = string
   description = "client id"
@@ -38,3 +69,12 @@ variable "subscriptionid" {
   default = "value"
 }
 
+
+############
+# Identity #
+############
+variable "msi_id" {
+  type        = string
+  description = "The Managed Service Identity ID. If this value isn't null (the default), 'data.azurerm_client_config.current.object_id' will be set to this value."
+  default     = null
+}
