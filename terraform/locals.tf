d4e63c24e6b579c87a2c4380f9437b16ed18af24
SathishMani219 <sathish.kumarmani@education.gov.uk>
2024-02-19 22:27:34 +0000

    terraform app container

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,23 +9,40 @@ locals {
   azure_location            = var.azure_location
   resource_prefix           = "${local.environment}${local.project_name}"
   azure_resource_group_name = var.resource_group_name
+  registry_server           = var.registry_server
+  registry_username         = var.registry_username
+  registry_password         = var.registry_password
+  registry_custom_image_url = var.registry_custom_image_url
 
 
   tags = {
     "Environment"      = var.az_tag_environment,
     "Service Offering" = var.az_tag_product,
     "Product"          = var.az_tag_product
-  }
-
-  ##################
-  # Azure KeyVault #
-  ##################
-  kv_name = "${local.environment}${local.project_name}-kv"
+ }
 
 
-  ###########
+    ########################
+    ### Container App ###
+    ########################
+
+   container_app_image_name = var.container_app_image_name
+   container_port           = var.az_container_port
+   kestrel_endpoint = var.az_app_kestrel_endpoint
+
+   ##################
+   # Azure KeyVault #
+   ##################
+   kv_name = "${local.environment}${local.project_name}-kv"
+
+
+   ###########
   # tfstate storage container #
-  ###########
-  tfstate_container_name = var.tfstate_storage_container_name
-  azurerm_terraform_storage_account = var.tf_state_storage_account
-}
+   ###########
+   tfstate_container_name = var.tfstate_storage_container_name
+   azurerm_terraform_storage_account = var.tf_state_storage_account
+
+  ####################
+  # Managed Identity #
+  ####################
+  user_identity_name = var.serviceprinciple_identity
