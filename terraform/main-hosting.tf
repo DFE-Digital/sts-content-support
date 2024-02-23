3a3e6f4e9ef9efe6633a64b5d4ea3a2d0c04c0df
SathishMani219 <sathish.kumarmani@education.gov.uk>
2024-02-23 11:30:59 +0000

    terraform jupdate

--- terraform/main-hosting.tf
+++ terraform/main-hosting.tf
@@ -58,13 +58,6 @@
   container_access_type = "private"
 }
 
-resource "azurerm_resource_group" "default" {
-  #count = local.existing_resource_group == "" ? 1 : 0
-
-  name     = local.resource_prefix
-  location = local.azure_location
-  tags     = local.tags
-}
 
 
 module "main_hosting" {
