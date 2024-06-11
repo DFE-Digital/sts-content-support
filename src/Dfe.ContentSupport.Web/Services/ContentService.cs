b6a448d5969af590602953055fcf99fb5531b36f
jack.coggin <jack.coggin@and.digital>
2024-06-11 15:34:31 +0100

    revert ContentService changes

--- src/Dfe.ContentSupport.Web/Services/ContentService.cs
+++ src/Dfe.ContentSupport.Web/Services/ContentService.cs
@@ -15,24 +15,19 @@ public class ContentService(IContentfulService contentfulService) : IContentServ
 
     public async Task<string> GenerateSitemap(string baseUrl)
     {
-        var resp = await GetContentSupportPages(
-            nameof(ContentSupportPage.IsSitemap),
-            "true",
-            false
-        );
+        var resp =
+            await GetContentSupportPages(nameof(ContentSupportPage.IsSitemap), "true", false);
 
         XNamespace xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9";
         var sitemap = new XDocument(
             new XDeclaration("1.0", "UTF-8", null),
-            new XElement(
-                xmlns + "urlset",
-                new XAttribute("xmlns", xmlns),
+            new XElement(xmlns + "urlset", new XAttribute("xmlns", xmlns),
                 from url in resp
-                select new XElement(
-                    xmlns + "url",
-                    new XElement(xmlns + "loc", $"{baseUrl}{url.Slug}"),
-                    new XElement(xmlns + "changefreq", "yearly")
-                )
+                select
+                    new XElement(xmlns + "url",
+                        new XElement(xmlns + "loc", $"{baseUrl}{url.Slug}"),
+                        new XElement(xmlns + "changefreq", "yearly")
+                    )
             )
         );
 
@@ -40,13 +35,9 @@ public class ContentService(IContentfulService contentfulService) : IContentServ
     }
 
     private async Task<ContentfulCollection<ContentSupportPage>> GetContentSupportPages(
-        string field,
-        string value,
-        bool isPreview
-    )
+        string field, string value, bool isPreview)
     {
-        var builder = QueryBuilder<ContentSupportPage>
-            .New.ContentTypeIs(nameof(ContentSupportPage))
+        var builder = QueryBuilder<ContentSupportPage>.New.ContentTypeIs(nameof(ContentSupportPage))
             .FieldEquals($"fields.{field}", value);
 
         return await contentfulService.ContentfulClient(isPreview).Query(builder);
