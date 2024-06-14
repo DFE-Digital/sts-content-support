4e9d798e95ea475d359d3b9514e34976f58617f7
Tom Whittington <tom.whittington@education.gov.uk>
2024-06-14 10:41:38 +0100

    Card cleanup

--- src/Dfe.ContentSupport.Web/Models/Target.cs
+++ src/Dfe.ContentSupport.Web/Models/Target.cs
@@ -1,8 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Reflection.Metadata.Ecma335;
-using Contentful.Core.Configuration;
 using Contentful.Core.Models;
-using Newtonsoft.Json;
 
 namespace Dfe.ContentSupport.Web.Models;
 
@@ -12,8 +9,6 @@ public class Target: Entry
     public Fields Fields { get; set; } = null!;
     public Sys Sys { get; set; } = null!;
     public string Title { get; set; } = null!;
-
-    [JsonConverter(typeof(AssetJsonConverter))]
     public Asset Asset { get; set; } = null!;
     public ContentType ContentType { get; set; } = null!;
     public string Description { get; set; } = null!;
