5d730046e0816461cc0ea0580c99b154e6107939
simonjfirth <simon.j.firth@gmail.com>
2024-06-28 12:04:08 +0100

    Added default header handling to _layout

--- src/Dfe.ContentSupport.Web/Controllers/HomeController.cs
+++ src/Dfe.ContentSupport.Web/Controllers/HomeController.cs
@@ -47,7 +47,6 @@ public class HomeController(IContentService contentService)
         {
             Heading = new Models.Heading
             {
-                Title = "",
                 Subtitle = "",
             }
         };
